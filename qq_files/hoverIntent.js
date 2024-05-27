194a44871a88b0e7d64249972c9a808b54ce707a
seemeego <webchiangmai@gmail.com>
2024-05-27 15:30:59 +0700

    Add files via upload

---
+++ qq_files/hoverIntent.js
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>Object not found!</title>
+<link rev="made" href="mailto:you@example.com" />
+<style type="text/css"><!--/*--><![CDATA[/*><!--*/ 
+    body { color: #000000; background-color: #FFFFFF; }
+    a:link { color: #0000CC; }
+    p, address {margin-left: 3em;}
+    span {font-size: smaller;}
+/*]]>*/--></style>
+</head>
+
+<body>
+<h1>Object not found!</h1>
+<p>
+
+
+    The requested URL was not found on this server.
+
+  
+
+    The link on the
+    <a href="http://192.168.64.2/beauty-health-01/39/papifeed-3-7l-%25E0%25B9%2580%25E0%25B8%2584%25E0%25B8%25A3%25E0%25B8%25B7%25E0%25B9%2588%25E0%25B8%25AD%25E0%25B8%2587%25E0%25B9%2583%25E0%25B8%25AB%25E0%25B9%2589%25E0%25B8%25AD%25E0%25B8%25B2%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%25A3%25E0%25B8%25AD%25E0%25B8%25B1%25E0%25B8%2595%25E0%25B9%2582%25E0%25B8%2599%25E0%25B8%25A1%25E0%25B8%25B1%25E0%25B8%2595%25E0%25B8%25B4-smart-pet-feeder-tuya-wifi-camera-%25E0%25B8%2595%25E0%25B8%25B1%25E0%25B9%2589%25E0%25B8%2587%25E0%25B9%2580%25E0%25B8%25A7%25E0%25B8%25A5%25E0%25B8%25B2%25E0%25B9%2583%25E0%25B8%25AB%25E0%25B9%2589%25E0%25B8%25AD%25E0%25B8%25B2%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%25A3%25E0%25B9%2584%25E0%25B8%2594%25E0%25B9%2589.html">referring
+    page</a> seems to be wrong or outdated. Please inform the author of
+    <a href="http://192.168.64.2/beauty-health-01/39/papifeed-3-7l-%25E0%25B9%2580%25E0%25B8%2584%25E0%25B8%25A3%25E0%25B8%25B7%25E0%25B9%2588%25E0%25B8%25AD%25E0%25B8%2587%25E0%25B9%2583%25E0%25B8%25AB%25E0%25B9%2589%25E0%25B8%25AD%25E0%25B8%25B2%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%25A3%25E0%25B8%25AD%25E0%25B8%25B1%25E0%25B8%2595%25E0%25B9%2582%25E0%25B8%2599%25E0%25B8%25A1%25E0%25B8%25B1%25E0%25B8%2595%25E0%25B8%25B4-smart-pet-feeder-tuya-wifi-camera-%25E0%25B8%2595%25E0%25B8%25B1%25E0%25B9%2589%25E0%25B8%2587%25E0%25B9%2580%25E0%25B8%25A7%25E0%25B8%25A5%25E0%25B8%25B2%25E0%25B9%2583%25E0%25B8%25AB%25E0%25B9%2589%25E0%25B8%25AD%25E0%25B8%25B2%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%25A3%25E0%25B9%2584%25E0%25B8%2594%25E0%25B9%2589.html">that page</a>
+    about the error.
+
+  
+
+</p>
+<p>
+If you think this is a server error, please contact
+the <a href="mailto:you@example.com">webmaster</a>.
+
+</p>
+
+<h2>Error 404</h2>
+<address>
+  <a href="/">192.168.64.2</a><br />
+  <span>Apache/2.4.46 (Unix) OpenSSL/1.1.1h PHP/7.4.12 mod_perl/2.0.11 Perl/v5.32.0</span>
+</address>
+</body>
+</html>
+
